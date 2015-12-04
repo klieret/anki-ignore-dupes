462bec491cd0967264bc0c1a590ab2607a48a6b7
ch4noyu <ch4noyu@yahoo.com>
2015-12-04 16:15:06 +0900

    added encoding coding

--- ignore_dupes.py
+++ ignore_dupes.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 from anki.notes import Note
 from anki.utils import fieldChecksum, splitFields, stripHTMLMedia
 from aqt import mw
 
 def ignore_duplicate(deck1, deck2):
-	""" 	Having 2 cards from deck1 (original deck) and deck2 
+	""" Having 2 cards from deck1 (original deck) and deck2 
 	(deck of a note which normally would be flagged as a duplicate) 
 	this function should be modified to return True if we want to ignore 
 	this duplicate and False if we want to flag the duplicate. """
 
-	noDupeDecks = ["SOURCES::japanese_kanji_system","SOURCES::Japanisch im Sauseschritt source", "SOURCES::RTK2_Public_source", "VOCAB::vocab_system"]
+	noDupeDecks = [ "SOURCES::japanese_kanji_system",
+					"SOURCES::Japanisch im Sauseschritt source", 
+					"SOURCES::RTK2_Public_source", 
+					"VOCAB::vocab_system"]
+	
 	if deck2 in noDupeDecks:
 		return False
 	else:
