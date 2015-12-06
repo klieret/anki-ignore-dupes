63c8cdd0e3c39bca760214a605cd158b014973aa
klieret <klieret@users.noreply.github.com>
2015-12-06 09:57:39 +0900

    fix: log gets created in addon subdirectory

--- ignore_dupes/log.py
+++ ignore_dupes/log.py
@@ -2,18 +2,21 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import os.path
 
 logger = logging.getLogger('ignore_dupes_logging')
 logger.setLevel(logging.DEBUG)
-
-fh = logging.FileHandler('ignore_dupes.log', mode="w")
-fh.setLevel(logging.DEBUG)
 
 # everything that is handled by a StreamHandler will
 # be caught by Anki and displayed in a warning window
 # so we only use this for ERROR and CRITICAL level.
 sh = logging.StreamHandler()
 sh.setLevel(logging.ERROR)
+
+addon_dir = os.path.dirname(__file__)
+log_path = os.path.join(addon_dir, 'ignore_dupes.log')
+fh = logging.FileHandler(log_path, mode="w")
+fh.setLevel(logging.DEBUG)
 
 logger.addHandler(fh)
 logger.addHandler(sh)
