5c9e97c9c99faccc4ef180a1e2c378ee5b404c33
klieret <klieret@users.noreply.github.com>
2015-12-06 09:51:57 +0900

    fixed bug, added stream handler

--- ignore_dupes/log.py
+++ ignore_dupes/log.py
@@ -6,7 +6,14 @@ import logging
 logger = logging.getLogger('ignore_dupes_logging')
 logger.setLevel(logging.DEBUG)
 
-fh = logging.FileHandler('ignore_dupes.log', filemode="w")
+fh = logging.FileHandler('ignore_dupes.log', mode="w")
 fh.setLevel(logging.DEBUG)
 
+# everything that is handled by a StreamHandler will
+# be caught by Anki and displayed in a warning window
+# so we only use this for ERROR and CRITICAL level.
+sh = logging.StreamHandler()
+sh.setLevel(logging.ERROR)
+
 logger.addHandler(fh)
+logger.addHandler(sh)
