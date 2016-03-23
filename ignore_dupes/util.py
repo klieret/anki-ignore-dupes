1a14dcab9f4d7fa5ec6bf232a8b13efd88e31296
klieret <klieret@users.noreply.github.com>
2016-03-23 22:34:37 +0900

    expression_dupe splits expression and checks parts

--- ignore_dupes/util.py
+++ ignore_dupes/util.py
@@ -15,3 +15,12 @@ def dname_from_did(deck_id):
     """ Return the deck name for the deck with the id $deck_name
     :type deck_id: int"""
     return mw.col.decks.get(deck_id)["name"]
+
+
+def split_multiple_delims(string, delims):
+    """ Like the string.split(...) method, but with
+    multiple delimeters.
+    See http://stackoverflow.com/questions/4998629/python-split-string-with-multiple-delimiters """
+    for delim in delims:
+        string.replace(delim, delims[0])
+    return string.split(delims[0])
